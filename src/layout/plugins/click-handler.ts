66a92cad237e0099318a1a4a93ea6d8be7f01890
duchangkim <duchang.dev@gmail.com>
2024-12-03 16:21:12 +0900

    ✨ Enhance click handling in layout plugin to support node selection

--- src/layout/plugins/click-handler.ts
+++ src/layout/plugins/click-handler.ts
@@ -1,6 +1,6 @@
 import { createNode } from '@/core';
 import { NodeType } from 'prosemirror-model';
-import { Plugin, PluginKey } from 'prosemirror-state';
+import { NodeSelection, Plugin, PluginKey } from 'prosemirror-state';
 
 export interface LayoutClickPluginConfig {
   layoutNodeType: NodeType;
@@ -13,6 +13,27 @@ export const layoutClickPlugins = (config: LayoutClickPluginConfig) => {
       key: new PluginKey('layoutClickHandler'),
       props: {
         handleDOMEvents: {
+          click: (view, event) => {
+            // dblclick 핸들러가 있어서 명시적으로 일반 클릭 이벤트 추가
+            const pos = view.posAtDOM(event.target as Node, 0);
+
+            if (pos === null) {
+              return false;
+            }
+
+            const $pos = view.state.doc.resolve(pos);
+            const node = $pos.node();
+
+            if (node.type.spec.selectable) {
+              const tr = view.state.tr.setSelection(
+                NodeSelection.create(view.state.doc, $pos.before()),
+              );
+              view.dispatch(tr);
+              return true;
+            }
+
+            return false;
+          },
           dblclick: (view, event) => {
             const pos = view.posAtDOM(event.target as Node, 0);
 
